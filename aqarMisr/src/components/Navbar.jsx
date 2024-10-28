218289c857a326d3d1c16037808e4ceca5e3577e
CodeCantaloupe <141568278+CodeCantaloupe@users.noreply.github.com>
2024-10-28 23:50:19 +0300

    Added Navbar Component

--- aqarMisr/src/components/Navbar.jsx
+++ aqarMisr/src/components/Navbar.jsx
@@ -1,8 +1,36 @@
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-white">
+    <nav className="w-full bg-white text-teal-900 text-nowrap h-28 flex justify-between items-center px-6">
+        {/* logo */}
+        <div>
+            <img className="size-16" src="/aqar_logo.png" alt="aqar misr logo"/>
+        </div>
 
+        {/* links */}
+        <div className="hidden lg:block text-lg font-[500]">
+            <Link className="p-4 hover:bg-gray-100 rounded-full transition" href="/">الرئيسية</Link>
+            <Link className="p-4 hover:bg-gray-100 rounded-full transition" href="/">دليل عقارات</Link>
+            <Link className="p-4 hover:bg-gray-100 rounded-full transition" href="/">عقاراتنا</Link>
+            <Link className="p-4 hover:bg-gray-100 rounded-full transition" href="/">أحدث المشاريع</Link>
+            <Link className="p-4 hover:bg-gray-100 rounded-full transition" href="/">لماذا عقار مصر</Link>
+            <Link className="p-4 hover:bg-gray-100 rounded-full transition" href="/">المدونة</Link>
+            <Link className="p-4 hover:bg-gray-100 rounded-full transition" href="/">تواصل معنا</Link>
+        </div>
+
+        {/* Add Prop & Phone number */}
+        <div className="flex items-center gap-10">
+            <div className="hidden lg:flex items-center gap-4">
+                <img className="size-5" src="phone_icon.svg" alt="phone number icon"/>
+                <label className="text-sm">0106 285 8443</label>
+            </div>
+
+            <button className="px-7 py-2 border border-black rounded-full hover:bg-teal-900 hover:text-white transition-colors">
+                <Link href="/add-property">اضافة عقار</Link>
+            </button>
+
+        </div>
     </nav>
   )
 }
--- aqarMisr/src/pages/index.js
+++ aqarMisr/src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <meta name="description" content="موقع عقار مصر يعرض كل ماهو جديد في عالم العقارات في مصر بشكل عام والعاصمة الادارية بشكل خاص" />
       </Head>
       <div>
-        Hello There!
+        
       </div>
     </>
   );
