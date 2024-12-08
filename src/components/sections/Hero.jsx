4989bc596cd2d5908e320708c3757e1f75123f5f
CodeCantaloupe <141568278+CodeCantaloupe@users.noreply.github.com>
2024-12-08 21:42:46 +0200

    Modified hero section responsiveness

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -12,7 +12,7 @@ import Link from "next/link";
  */
 const Hero = () => {
   return (
-    <div className='h-[calc(100vh-112px)] bg-[url("/hero_bg.png")] flex flex-col p-6 space-y-8 items-center bg-no-repeat bg-contain bg-center text-teal-900'>
+    <div className='h-[calc(100vh-110px)] bg-[url("/hero_bg.png")] flex flex-col p-6 space-y-8 items-center bg-no-repeat bg-cover bg-left-bottom text-teal-900'>
         <p className="border border-black px-6 py-2 rounded-full">أحدث المشاريع</p>
         
         <p className="text-center">موقع عقار مصر يعرض كل ماهو جديد في عالم العقارات في مصر بشكل عام والعاصمة الادارية بشكل خاص</p>
