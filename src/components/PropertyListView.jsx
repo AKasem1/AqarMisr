fd384931db71c3c3cd451606b1aae77bca0e1623
AKasem1 <abdelrahmankasem7@gmail.com>
2024-12-16 00:46:37 +0200

    fixing build error

--- src/components/PropertyListView.jsx
+++ src/components/PropertyListView.jsx
@@ -6,7 +6,7 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel";
+} from "@/components/UI/carousel";
 import axios from "axios";
 import { data } from "react-router-dom";
 
